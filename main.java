f1d0b550b718c2903cd554cf6fc47c33e31e61d5
Emilioch <rechaac@hotmail.com>
2024-07-14 14:16:32 -0300

    modificaando clase main

--- main.java
+++ main.java
@@ -8,24 +8,32 @@ public class main {
        ArrayList<Persona>lista= new ArrayList<>(); 
        int cp=0;
        
-        while(flag==false){
-           Persona per= new Persona();
-           per.cargarDatos();
-           lista.add(per);
-             
-             System.out.println("desea continuar con la carga s/n");
-             resp=tec.next().charAt(0);
 
-             if(resp=='n'){
-                flag=true;
-             }
-            
-           }
+       while(flag==false){
+        Persona per= new Persona();
+        per.cargarDatos();
+        lista.add(per);
+          
+          System.out.println("desea continuar con la carga s/n");
+          resp=tec.next().charAt(0);
+   
+          if(resp=='n'){
+             flag=true;
+          }
+         
+        }
+       
             for(Persona Lista:lista){
                 System.out.println("Nombre: "+Lista.getNombre() + "\n"+"Edad: "+Lista.getEdad()+"\n"+"Apellido :"+Lista.getApellido()+"\n"+"Direccion: "+Lista.getDireccion());
                 System.out.println("----------------------------------");
                 cp+=1;
             }
               System.out.println("La cantidad de personas es de: "+cp);
-    }
+    
+
+
+       
+ 
+    
+  }
 }
