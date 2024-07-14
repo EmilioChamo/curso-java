5810e6302744cccbdfae2fa560ea4778c53979f5
Emilioch <rechaac@hotmail.com>
2024-07-14 15:51:32 -0300

    aplicando switch case

--- main.java
+++ main.java
@@ -2,17 +2,18 @@ import java.util.ArrayList;
 import java.util.Scanner;
 public class main {
     public static void main (String[]args){
-       boolean menu,flag=false;
+       boolean flag=false;
+       boolean menu=false;
        Scanner tec = new Scanner(System.in);
        char resp;
        ArrayList<Persona>lista= new ArrayList<>(); 
        int cp=0;
        int op;
        
-
+       while (menu==false){
        System.out.println("Binvenido al sistema"+"\n"+ "1:Carga"+"\n"+"2:Busqueda"+"\n"+"3:Modificacion"+"\n"+"4:Eliminacion"+"\n"+"5:Salir");
            op=tec.nextInt();
-
+         
            switch(op){
             case 1:
               while(flag==false){
@@ -26,7 +27,9 @@ public class main {
                 if(resp=='n'){
                    flag=true;
                 }
+                
               }
+            
               break;
               case 2:
               char opcion;
@@ -35,7 +38,8 @@ public class main {
                 opcion= tec.next().charAt(0);
                        if(opcion=='n'){
                         System.out.print("ingrese el nombre a buscar por favor: ");
-                        String nombre=tec.nextLine();
+                        String nombre;
+                        nombre=tec.nextLine();
                                
                            for(Persona Lista:lista){
                              if(Lista.getNombre()==nombre){
@@ -45,7 +49,8 @@ public class main {
                            }
                        }else if(opcion=='a'){
                         System.out.print("ingrese el apellido a buscar por favor: ");
-                        String apellido=tec.nextLine();
+                        String apellido;
+                        apellido=tec.nextLine();
                         for(Persona Lista:lista){
                           if(Lista.getNombre()==apellido){
                            System.out.println("Nombre: "+Lista.getNombre() + "\n"+"Edad: "+Lista.getEdad()+"\n"+"Apellido :"+Lista.getApellido()+"\n"+"Direccion: "+Lista.getDireccion());
@@ -53,20 +58,24 @@ public class main {
                           }
                         }
                        }
-
-
+                       break;
+                case 5:
+                 System.out.println("Gracias por usar nuestro sistema");
+                    menu=true;       
+                       break;
               }
-           
+            }
+
 
        
-       
+           /* 
             for(Persona Lista:lista){
                 System.out.println("Nombre: "+Lista.getNombre() + "\n"+"Edad: "+Lista.getEdad()+"\n"+"Apellido :"+Lista.getApellido()+"\n"+"Direccion: "+Lista.getDireccion());
                 System.out.println("----------------------------------");
                 cp+=1;
             }
               System.out.println("La cantidad de personas es de: "+cp);
-    
+            */
 
 
        
